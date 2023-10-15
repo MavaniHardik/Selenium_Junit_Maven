fdda929325ff27b092f244ead3df8f7cd47f20c6
MavaniHardik <hardikmavani96@gmail.com>
2023-10-15 17:59:05 +0100

    all

--- src/test/java/org/example/testFunction/RadioButton.java
+++ src/test/java/org/example/testFunction/RadioButton.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RadioButton {
