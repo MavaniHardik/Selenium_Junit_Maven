5dcf6794bd6862e77ac70b92d119020419176ca2
MavaniHardik <hardikmavani96@gmail.com>
2023-10-26 17:08:48 +0100

    Merge remote-tracking branch 'origin/RadioButton' into RadioButton
    # Conflicts:
    #	src/test/java/org/example/DemoNopRegistration.java

--- src/test/java/org/example/Selenium/Youtube.java
+++ src/test/java/org/example/Selenium/Youtube.java
@@ -30,7 +30,7 @@ public class Youtube {
         Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@id=\"search\"]")).sendKeys("Udja kale kava");
         driver.findElement(By.id("search-icon-legacy")).click();
-
+//Not Play the song after below X path
       Thread.sleep(5000);
       driver.findElement(By.xpath("//*[@aria-label=\"Udja Kale Kawan | Gadar | Sunny Deol & Ameesha Patel | Alka Yagnik & Udit Narayan | Lyrical by Vaani vaani 2,861,960 views 4 months ago 7 minutes, 33 seconds\"]")).click();
     }
